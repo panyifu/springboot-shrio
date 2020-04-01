d7593811f200fb38494970506e69c9a61433b7ba
Administrator <Administrator@192.168.230.2>
2020-04-02 00:26:04 +0800

    demo

--- src/main/java/com/shiro/shirodemo/Application.java
+++ src/main/java/com/shiro/shirodemo/Application.java
@@ -19,5 +19,6 @@ import org.springframework.web.filter.CorsFilter;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
 	}
 }
